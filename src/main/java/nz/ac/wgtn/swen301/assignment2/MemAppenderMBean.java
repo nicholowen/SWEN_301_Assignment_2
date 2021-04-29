0fbff55c016a287cdef90f88d33ed903c15c875f
Owen Nicholson <nicholowen@gmail.com>
2021-04-29 20:16:57 +1200

    Add MBean interface.

---
+++ src/main/java/nz/ac/wgtn/swen301/assignment2/MemAppenderMBean.java
@@ -0,0 +1,10 @@
+package nz.ac.wgtn.swen301.assignment2;
+
+public interface MemAppenderMBean {
+
+    String[] getLogs();
+    long getLogCount();
+    long getDiscardedLogCount();
+    void exportToJSON(String filename);
+
+}
